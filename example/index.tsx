52868567d779315664d1a53b7e99bd4632a131d9
harish.kulkarni <harish.kulkarni@sezzle.com>
2021-03-12 01:10:44 +0530

    Hello, This is Stars rating playground, made it for fun!

--- example/index.tsx
+++ example/index.tsx
@@ -1,13 +1,45 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { StarsRating } from '../.';
+import './index.css';
 
 const App = () => {
+  const config = {
+    number: 5,
+    value: 2,
+    renderFull:'★',
+    renderEmpty: '☆',
+  }
+
+  const config1 = {
+    number: 5,
+    value: 2,
+    renderFull: <span className='mr-10 full'>A</span>,
+    renderEmpty: <span className='mr-10 empty'>A</span>
+  }
+
+  const config2 = {
+    number: 5,
+    value: 2,
+    renderFull: '✅',
+    renderEmpty: '☑'
+  }
+
   return (
-    <div>
-      <Thing />
+    <>
+    <div className='wrapper'>
+      <StarsRating config={config} />
     </div>
+
+    <div className='wrapper'>
+      <StarsRating config={config1} />
+    </div>
+
+    <div className='wrapper'>
+      <StarsRating config={config2} />
+    </div>
+    </>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,72 @@
 import * as React from 'react';
+import './styles/StarRating.css';
 
-// Delete me
-export const Thing = () => {
-  return <div>they snozzberries taste like snozzberries</div>;
+type ConfigType = {
+  number: number;
+  value?: number;
+  renderFull: JSX.Element | string;
+  renderHalf?: JSX.Element | string;
+  renderEmpty?: JSX.Element | string;
+};
+
+type Props = {
+  config: ConfigType;
+};
+
+export const StarsRating = (props: Props) => {
+  const {
+    number,
+    value = 0,
+    renderFull,
+    renderHalf = renderFull,
+    renderEmpty = '',
+  } = props.config;
+
+  const [selected, setSelected] = React.useState(value);
+  const [softselected, setSoftSelected] = React.useState(value);
+
+  const renderStar = (n: number) => {
+    if (n < 0) return null;
+
+    const onClick = (newVal: number) => {
+      setSelected(newVal);
+    };
+
+    const onMouseOut = () => {
+      setSoftSelected(selected);
+    };
+
+    const onMouseMove = (e: any, index: number) => {
+      const rect = e.target.getBoundingClientRect();
+      const xmiddleRelativePosistion = e.pageX - e.target.offsetLeft;
+
+      if (rect.width / 2 <= xmiddleRelativePosistion) {
+        setSoftSelected(index + 0.5);
+      } else {
+        setSoftSelected(index);
+      }
+    };
+
+    const stars = Array.from(Array(number), (_, i: number) => {
+      let sindex = i + 0.5;
+      const isSelected = sindex <= softselected;
+      const isHalf = Number.isInteger(softselected);
+
+      return (
+        <span
+          key={i}
+          onMouseOut={() => onMouseOut()}
+          onMouseMove={e => onMouseMove(e, sindex)}
+          onClick={() => onClick(sindex)}
+          className="star"
+        >
+          {isSelected ? (isHalf ? renderHalf : renderFull) : renderEmpty}
+        </span>
+      );
+    });
+
+    return stars;
+  };
+
+  return <span className="stars">{renderStar(number)}</span>;
 };
