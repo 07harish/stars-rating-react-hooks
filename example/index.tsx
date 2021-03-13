8f2129b387d9679033587db555175ffd97817f00
07harish <07harish96@gmail.com>
2021-03-13 15:46:10 +0530

    update example

--- example/index.tsx
+++ example/index.tsx
@@ -32,12 +32,39 @@ const App = () => {
     renderFull: '✅',
     renderEmpty: '☑',
   };
+
   const config3 = {
     number: 5,
     value: 3.5,
     renderFull: <i className="fas fa-star"></i>,
     renderEmpty: <i className="far fa-star"></i>,
     renderHalf: <i className="fas fa-star-half-alt"></i>,
+  };
+
+  const config4 = {
+    number: 5,
+    value: 3.5,
+    renderFull: <img src="https://img.icons8.com/fluent/48/000000/star.png" />,
+    renderEmpty: (
+      <img src="https://img.icons8.com/color/48/000000/star--v1.png" />
+    ),
+    renderHalf: (
+      <img src="https://img.icons8.com/fluent/48/000000/star-half-empty.png" />
+    ),
+  };
+
+  const config5 = {
+    number: 5,
+    value: 4.5,
+    renderFull: (
+      <img src="https://img.icons8.com/ios-filled/50/000000/star--v1.png" />
+    ),
+    renderEmpty: (
+      <img src="https://img.icons8.com/ios/50/000000/star--v1.png" />
+    ),
+    renderHalf: (
+      <img src="https://img.icons8.com/ios-filled/50/000000/star-half-empty.png" />
+    ),
   };
 
   const { stars, getStarProps, getStarWrapperProps, isSelecting } = useStars(
@@ -48,41 +75,37 @@ const App = () => {
     <div className="app flex-col">
       <div className="headingWrapper">
         <h1 className="heading">stars-rating-react-hooks</h1>
-      </div>
-
-      <div className="wrapper">
-        <div>{isSelecting ? 'Selecting' : null}</div>
-        <StarsRating config={config} />
-      </div>
-
-      <div className="wrapper">
-        <StarsRating config={config1} />
-      </div>
-
-      <div className="wrapper">
-        <StarsRating config={config2} />
-      </div>
-
-      <div className="wrapper  flex-col">
-        <h5>{selecting.isSelecting ? 'Rating...' : 'Rate'}</h5>
-        <h3>{selecting.selectingValue}</h3>
         <h3>
+          {selecting.selectingValue}{' '}
+          {selecting.isSelecting ? 'Rating...' : 'Stars'}
+        </h3>
+        {/* <h3>
           <span className="mr-10">
             Last selected: {SelectedValue || config3.number}
           </span>
           &nbsp; &nbsp; &nbsp; &nbsp;
           <span>Selecting: {selecting.selectingValue}</span>
-        </h3>
+        </h3> */}
         <StarsRating
           isSelecting={(isSelecting, selectingValue) => {
             setselecting({ isSelecting, selectingValue });
           }}
           onStarsRated={selectedValue => {
             setSelectedValue(selectedValue);
+          }}
+          config={config5}
+        />
+      </div>
+
+      <div className="wrapper">
+        <StarsRating
+          onStarsRated={selectedValue => {
+            setSelectedValue(selectedValue);
             alert(`You just rated ${selectedValue} stars 🎉`);
           }}
-          config={config3}
+          config={config4}
         />
+        <StarsRating config={config3} />
       </div>
 
       <div className="wrapper flex-col">
@@ -96,6 +119,13 @@ const App = () => {
             ))}
           </span>
         </div>
+      </div>
+      <div className="wrapper">
+        <StarsRating config={config1} />
+      </div>
+
+      <div className="wrapper">
+        <StarsRating config={config2} />
       </div>
     </div>
   );
